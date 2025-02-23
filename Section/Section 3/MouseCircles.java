8106623352b96228227198f2acb83f961757edf6
vantdo222 <dothuyvanhb@gmail.com>
2025-02-23 15:39:58 -0700

    Add files via upload

---
+++ Section/Section 3/MouseCircles.java
@@ -0,0 +1,34 @@
+/*
+ * File: MouseCircles.java
+ * ------------------------
+ * This program should draw a circle each time the mouse is clicked,
+ * centered where the click occurs.  Each circle has a randomly
+ * chosen color and a randomly chosen radius between MIN_RADIUS
+ * and MAX_RADIUS pixels.
+ */
+
+import java.awt.event.MouseEvent;
+
+import acm.program.*;
+import acm.graphics.*;
+import acm.util.*;
+
+public class MouseCircles extends GraphicsProgram {
+
+	/** Minimum radius */
+	private static final double MIN_RADIUS = 5;
+
+	/** Maximum radius */
+	private static final double MAX_RADIUS = 50;
+
+	public void run() {
+		// TODO: add your code here
+	}
+
+	private void drawCircle(double x, double y) {
+		// TODO: add your code here
+	}
+			
+	/* Private instance variable */
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+}
